5b8fde3407abb6a7a73b1fd211b5c9a69be00e8e
CMB <cmb@pirdboydeimac.local>
2016-08-11 19:52:21 +0800

    Initial Commit

---
+++ Keep/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Keep
+//
+//  Created by CMB on 16/8/11.
+//  Copyright © 2016年 CMB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
